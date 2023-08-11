6797d603bf5a09998535073a9bcd55566d46c3a5
victorteixeiraifgoiano <victor.teixeira@estudante.ifgoiano.edu.br>
2023-08-11 14:45:00 -0300

    modulo de cadastro, alteracao e deletar produtos

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 
+from decimal import Decimal
+from flask import render_template
 import mysql.connector
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 
@@ -76,13 +78,17 @@ def dashboard():
         cursor = db.cursor()
         cursor.execute("SELECT * FROM users WHERE id = %s", (user_id,))
         user = cursor.fetchone()
-        cursor.close()
         
         if user:
-            return render_template('dashboard.html', current_user=user)
+            # Recupere a lista de produtos do banco de dados
+            cursor.execute("SELECT * FROM products")
+            products = cursor.fetchall()
+            
+            return render_template('dashboard.html', current_user=user, products=products)
     
     flash('Faça login para acessar o dashboard.', 'warning')
     return redirect(url_for('login'))
+
 
 # Rota do Logout
 @app.route('/logout')
@@ -90,6 +96,80 @@ def logout():
     session.pop('user_id', None)
     flash('Logout bem-sucedido.', 'info')
     return redirect(url_for('index'))
+
+
+# Rota para listar produtos
+@app.route('/products')
+def products():
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM products")
+    products = cursor.fetchall()
+    cursor.close()
+    return render_template('products.html', products=products)
+
+
+# Dentro do arquivo app.py
+
+
+# Rota para adicionar produto
+@app.route('/add_product', methods=['GET', 'POST'])
+def add_product():
+    if request.method == 'POST':
+        product_name = request.form['product_name']
+        product_description = request.form['product_description']
+        product_price = Decimal(request.form['product_price'].replace(',', '.'))  # Converter formato
+
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO products (name, description, price) VALUES (%s, %s, %s)",
+                       (product_name, product_description, product_price))
+        db.commit()
+        cursor.close()
+
+        flash('Produto adicionado com sucesso!', 'success')
+        return redirect(url_for('dashboard'))
+
+    return render_template('add_product.html')
+
+
+
+# Rota para editar produto
+@app.route('/edit_product/<int:product_id>', methods=['GET', 'POST'])
+def edit_product(product_id):
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM products WHERE id = %s", (product_id,))
+    product = cursor.fetchone()
+
+    if product:
+        if request.method == 'POST':
+            # Código para atualizar o produto, igual ao anterior
+            product_name = request.form['product_name']
+            product_description = request.form['product_description']
+            product_price = Decimal(request.form['product_price'].replace(',', '.'))  # Converter formato
+
+            cursor.execute("UPDATE products SET name = %s, description = %s, price = %s WHERE id = %s",
+                           (product_name, product_description, product_price, product_id))
+            db.commit()
+
+            flash('Alterações salvas com sucesso!', 'success')
+            return redirect(url_for('dashboard'))
+
+        return render_template('edit_product.html', product=product)
+
+    flash('Produto não encontrado.', 'danger')
+    return redirect(url_for('dashboard'))
+
+
+# Rota para deletar produto
+@app.route('/delete_product/<int:product_id>', methods=['POST'])
+def delete_product(product_id):
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM products WHERE id = %s", (product_id,))
+    db.commit()
+
+    flash('Produto excluído com sucesso!', 'success')
+    return redirect(url_for('dashboard'))
+
+
 
 
 
--- comandos.sql
+++ comandos.sql
@@ -11,3 +11,9 @@ CREATE TABLE users (
 
 
 
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL
+);
